724347b92434f6eef2976d593dee074d01663482
TaroSh0guN <zach.altuna03@gmail.com>
2025-03-29 15:58:57 -0400

    put node_modules and package-lock.json into .gitignore; set up initial things and used express to test

---
+++ app.js
@@ -0,0 +1,12 @@
+/**
+ * For local testing purposes
+ */
+
+import express from 'express';
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+});
